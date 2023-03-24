c53973e204588e677d22330d274a1550db73a000
leonardo <llopez@sigaba.io>
2023-03-24 00:25:36 -0600

    v1

---
+++ dev/src/components/ProductosSection.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <div class="productosSection">
+    <h2>PRODUCTOS</h2>
+
+    <img src="@/assets/nosotros/B-2.jpg" alt="Catre">
+  </div>
+</template>
+
+<style scoped>
+.productosSection{
+  text-align: center;
+  padding: 16px;
+  background-color: rgb(235, 232, 232);
+  font-weight: 400;
+}
+img{
+  margin-top: 16px;
+  width: 100%;
+}
+@media (min-width: 1024px) {
+
+}
+</style>
