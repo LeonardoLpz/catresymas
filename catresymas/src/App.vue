fb5bf0fb86897defb3982d0b920de97e11097a2f
leonardo <llopez@sigaba.io>
2023-03-23 18:26:16 -0600

    header

--- catresymas/src/App.vue
+++ catresymas/src/App.vue
@@ -1,47 +1,38 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <img 
+      alt="logo" 
+      class="logo" 
+      src="./assets/logogrande.png"
+    />
   </header>
+  <div class="divider">
 
+  </div>
   <main>
-    <TheWelcome />
+      <HelloWorld msg="Leonardo!"/>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+header{
+  padding: 16px 16px 10px 16px;
+}
+.logo{
+  width: 200px;
+}
+.divider{
+  height: 17px;
+  width: 100%;
+  background-color: rgb(239, 127, 0);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
